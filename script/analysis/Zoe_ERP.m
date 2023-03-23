e8276b6898a5bc54c6de4a386f1711163cc27760
Tzu-Han Zoe Cheng <tzcheng@ucsd.edu>
2023-03-23 00:30:47 -0700

    Update the reverse polarity

--- script/analysis/Zoe_ERP.m
+++ script/analysis/Zoe_ERP.m
@@ -49,8 +49,8 @@ unstable_tapper = find(stds > median(stds)); % use std of ITI
 
 % File path 
 addpath(genpath('/Volumes/TOSHIBA/Research/Imagined_beats/script'))
-% EEG_path = '/Volumes/TOSHIBA/Research/Imagined_beats/real_exp/preprocessed/icap/AM4b/rdlisten/all'; 
-EEG_path = '/Volumes/TOSHIBA/Research/Imagined_beats/real_exp/preprocessed/icap/AM4b/spontap/all'; 
+EEG_path = '/Volumes/TOSHIBA/Research/Imagined_beats/real_exp/preprocessed/icap/AM4b/rdlisten/all'; 
+% EEG_path = '/Volumes/TOSHIBA/Research/Imagined_beats/real_exp/preprocessed/icap/AM4b/spontap/all'; 
   
 cd(EEG_path)
 % files = dir(fullfile(EEG_path,'*sync3s_e.set')); 
@@ -65,7 +65,7 @@ aIC = [1	2	1	1	1	1	1	2	1	2	1	1	1	1	1	1	1	1	2	1	1	1	1	1	1];
 mIC = [2	1	2	2	2	2	2	1	2	1	2	2	2	2	2	2	2	2	1	2	2	2	2	2	2];
 
 %% Extract localizer individual's ERP
-reverse_polarity_aIC = [2 3 5 9 11 15 17 19 20 24]; % these are the right reverse as in STUDY
+reverse_polarity_aIC = [2 3 5 9 11 15 17 19 20 23 24]; % these are the right reverse as in STUDY
 reverse_polarity_mIC = [2 3 19 20 23]; % these are the right reverse as in STUDY
 
 for nsub = 1:length(names)
@@ -75,10 +75,10 @@ for nsub = 1:length(names)
 %    EEG = pop_eegfiltnew(EEG, 'hicutoff',60); % Note: this is only perform on EEG.data but not EEG.icaact
 %    EEG.icaact = EEG.icaweights*EEG.icasphere*EEG.data(EEG.icachansind,:); 
 %    EEG = eeg_checkset( EEG );
-    if ismember(nsub,reverse_polarity_mIC) 
-        erps(nsub,:) = -1*squeeze(mean(EEG.icaact(mIC(nsub),:,:),3));
+    if ismember(nsub,reverse_polarity_aIC) 
+        erps(nsub,:) = -1*squeeze(mean(EEG.icaact(aIC(nsub),:,:),3));
     else
-        erps(nsub,:) = squeeze(mean(EEG.icaact(mIC(nsub),:,:),3));
+        erps(nsub,:) = squeeze(mean(EEG.icaact(aIC(nsub),:,:),3));
     end
 %    figure;topoplot(EEG.icawinv(:,mIC(nsub)),EEG.chanlocs)
     title(strcat('sub = ',num2str(nsub)))
@@ -87,15 +87,13 @@ for nsub = 1:length(names)
     clear EEG parts_cleanEEG tempEEG
 end
 
-save mIC_erp_tap erps times
+save aIC_erp_rdlisten erps times
 figure;plot(times,erps);hold on;plot(times,mean(erps,1),'LineWidth',3,'color','k');xlim([-300 500])
 figure;plot(times,mean(erps(stable_tapper,:),1),'LineWidth',2);hold on; plot(times,mean(erps(unstable_tapper,:),1),'LineWidth',3);
 legend('Stable','Unstable')
 xlim([-300 500])
 ylabel('Amplitude')
 xlabel('Time (ms)')
-
-
 
 %%
 %erps(23,:) = erps(23,:)*-1;
